4fdb285658606847a4d3738562e61a0448824e8b
Maina32 <106369284+Maina32@users.noreply.github.com>
2025-03-29 17:35:20 +0300

    Delete CAT 2 INTERNET PROGRAMMING directory

--- CAT 2 INTERNET PROGRAMMING/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit/', views.submit_obituary, name='submit_obituary'),
-    path('', views.view_obituaries, name='view_obituaries'),
-    path('<slug:slug>/', views.obituary_detail, name='obituary_detail'),
-]
